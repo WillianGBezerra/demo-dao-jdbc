a7645483bfc0dde79e10b22ab369a674e5d5f50d
williangbezerra <williangomesbezerra@gmail.com>
2019-12-21 14:17:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1,"Books");
 		System.out.println(obj);
+		
+		Seller obj1 = new Seller(21,"Bob", "bob@gmail",new Date(), 3000.0, obj);
+		System.out.println(obj1);
 
 		
 	}
