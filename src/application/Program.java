ddf735553cdf1e6aea8a0f42a9d0c146c8cac0c7
williangbezerra <williangomesbezerra@gmail.com>
2019-12-25 23:59:12 -0300

    Scanner user interaction

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -12,17 +14,20 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException{
 		
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TESTE 1: seller findById ===");
-		Seller seller = sellerDao.findById(5);
+		System.out.println("Enter the Id's seller to search: ");
+		Seller seller = sellerDao.findById(sc.nextInt());
 		System.out.println(seller);
 
 		System.out.println("\n=== TESTE 2: seller findByDepartment ===");
-		Department department = new Department(2, null);
+		System.out.println("Enter the Id's department to search: ");
+		Department department = new Department(sc.nextInt(), null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
@@ -35,27 +40,39 @@ public class Program {
 		}
 		
 		System.out.println("\n=== TESTE 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		System.out.println("Enter seller name: ");
+		String name = sc.next();
+		System.out.println("Enter seller email: ");
+		String email = sc.next();
+		System.out.println("Enter seller birthdate: ");
+		Date birthDate = sdf.parse(sc.next());
+		System.out.println("Enter seller salary: ");
+		double baseSalary = sc.nextDouble(); 
+		System.out.println("Enter seller department: ");
+		department = new Department(sc.nextInt(), null);
+		Seller newSeller = new Seller(null, name, email,new java.sql.Date(birthDate.getTime()), baseSalary, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = "+ newSeller.getId());
 		
 		System.out.println("\n=== TESTE 5: seller update ===");
-		seller = sellerDao.findById(1);
-		seller.setName("Marta Waine");
+		System.out.println("Enter the Id: ");
+		seller = sellerDao.findById(sc.nextInt());
+		System.out.println("Enter seller name to update: ");
+		seller.setName(sc.next());
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
 		System.out.println("\n=== TESTE 6: seller delete ===");
 		System.out.println("Enter id for delete test: ");
-		int id = sc.nextInt();
-		sellerDao.deleteById(id);
+		sellerDao.deleteById(sc.nextInt());
 		System.out.println("Deleted completed");
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		System.out.println("=== TESTE 7: department findById ===");
-		department = departmentDao.findById(2);
+		System.out.println("Enter the id to search: ");
+		department = departmentDao.findById(sc.nextInt());
 		System.out.println(department);
-
+		
 		System.out.println("\n=== TESTE 8: seller findAll ===");
 		List<Department> listDep = departmentDao.findAll();
 		for (Department obj : listDep) {
@@ -63,23 +80,23 @@ public class Program {
 		}
 
 		System.out.println("\n=== TESTE 9: department insert ===");
-		department = new Department(null, "Storebooks");
+		System.out.println("Enter new department: ");
+		department = new Department(null,sc.next());
 		departmentDao.insert(department);
-		System.out.println("Inserted! New id = " + department.getId());
+		System.out.println("Inserted! New id = " + department.getId()+"!");
 
 		System.out.println("\n=== TESTE 10: department update ===");
 		System.out.println("Enter id for Update name: ");
 		department = departmentDao.findById(sc.nextInt());
-		System.out.println("Enter name for Update");
+		System.out.println("Enter name: ");
 		department.setName(sc.next());
 		departmentDao.update(department);
-		System.out.println("Update completed");
+		System.out.println("Update completed!");
 
 		System.out.println("\n=== TESTE 11: department delete ===");
 		System.out.println("Enter id for delete test: ");
-		id = sc.nextInt();
-		departmentDao.deleteById(id);
-		System.out.println("Deleted completed");
+		departmentDao.deleteById(sc.nextInt());
+		System.out.println("Deleted completed!");
 		
 		sc.close();
 	}
--- src/application/Program2.java
+++ src/application/Program2.java
@@ -14,7 +14,8 @@ public class Program2 {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		System.out.println("=== TESTE 7: department findById ===");
-		Department department = departmentDao.findById(2);
+		System.out.println("Enter the id to search: ");
+		Department department = departmentDao.findById(sc.nextInt());
 		System.out.println(department);
 
 		System.out.println("\n=== TESTE 8: seller findAll ===");
@@ -24,22 +25,22 @@ public class Program2 {
 		}
 
 		System.out.println("\n=== TESTE 9: department insert ===");
-		department = new Department(null, "Storebooks");
+		System.out.println("Enter new department: ");
+		department = new Department(null,sc.next());
 		departmentDao.insert(department);
-		System.out.println("Inserted! New id = " + department.getId());
+		System.out.println("Inserted! New id = " + department.getId()+"!");
 
 		System.out.println("\n=== TESTE 10: department update ===");
 		System.out.println("Enter id for Update name: ");
 		department = departmentDao.findById(sc.nextInt());
-		System.out.println("Enter name for Update");
+		System.out.println("Enter name: ");
 		department.setName(sc.next());
 		departmentDao.update(department);
-		System.out.println("Update completed");
+		System.out.println("Update completed!");
 
 		System.out.println("\n=== TESTE 11: department delete ===");
 		System.out.println("Enter id for delete test: ");
-		int id = sc.nextInt();
-		departmentDao.deleteById(id);
-		System.out.println("Deleted completed");
+		departmentDao.deleteById(sc.nextInt());
+		System.out.println("Deleted completed!");
 		sc.close();
 	}}
